1d666b44aaf332cce5ee2b1267474b0ea66edecf
Troy mangrum <tmmangrum@gmail.com>
2020-05-25 20:39:01 -0500

    a little behind, trying to follow props and other react needs

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,19 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
 
 const PostsPage = () => {
   // set up state for your data
+  const [posts, newPosts] = useState();
+
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {posts.map(post => (
+        <Post key={post.id} post={post} propTwo ={true} propThree={newPosts}/>
+      ))}
     </div>
   );
 };
